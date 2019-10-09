8b7626a23c0672afb9753ca7a5b8813d6a8f4e37
? <alberto.garcia.2016@upb.edu.co>
2019-10-09 12:51:17 -0500

    A

---
+++ src/models/message.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+});
+
+export default Message;
